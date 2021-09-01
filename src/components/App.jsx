4bb9bdca9d5de76683d7db8edb1bf30fcac47e1a
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-01 09:26:17 +0300

    heroStart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import '../style/App.css';
 import Header from './Layout/Header';
+import Hero from './Sections/Hero';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Sections/Hero.jsx
@@ -0,0 +1,4 @@
+import css from './Hero.module.css';
+export default function Hero() {
+  return <section>hero</section>;
+}
