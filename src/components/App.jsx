83655e6984cb46ca7186fa944c68ccf365d58432
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-02 15:22:26 +0300

    Layout in use, HeaderScroll start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,21 @@
 import '../style/App.css';
 import 'font-awesome/css/font-awesome.css';
-import Header from './Layout/Header';
 import Hero from './Sections/Hero';
 import Collections from './Sections/Collections/Collections';
 import Shop from './Sections/Shop/Shop';
 import Blog from './Sections/Blog/Blog';
 import Cta from './Sections/CTA/Cta';
+import Layout from './Layout/Layout';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
+    <Layout>
       <Hero />
       <Collections />
       <Shop />
       <Blog />
       <Cta />
-    </div>
+    </Layout>
   );
 }
 
