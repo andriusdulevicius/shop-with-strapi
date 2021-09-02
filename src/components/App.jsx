ae2753fee002e98f7df84a43df9aba2327319eea
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-02 14:58:36 +0300

    default props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,12 @@ function App() {
       {/* <Collections /> */}
       {/* <Shop /> */}
       {/* <Blog /> */}
-      <Cta title='Sub now' />
+      <Cta
+      // title='Subscribe Now'
+      // subtitle='Subscribe to Our Newsletter to get Important News, Amazing
+      //   Offers and Inside Scoops:'
+      // bg='https://assets.codepen.io/6093409/river.mp4'
+      />
     </div>
   );
 }
--- src/components/Sections/CTA/Cta.jsx
+++ src/components/Sections/CTA/Cta.jsx
@@ -1,19 +1,27 @@
 import css from './Cta.module.css';
 import Form from './Form';
 
-export default function Cta() {
+function Cta({ bg, title, subtitle }) {
   // Cta() gauti ivesties lauko reiskme is <Form />
   function handleEmail(emailInput) {
     console.log(emailInput);
   }
   return (
     <section className={css.cta}>
+      <video
+        className={css.video}
+        id='background-video'
+        autoPlay
+        loop
+        muted
+        poster='https://assets.codepen.io/6093409/river.jpg'
+      >
+        <source src={bg} type='video/mp4' />
+      </video>
+
       <div className={css.center}>
-        <h2>Subscribe Now</h2>
-        <p>
-          Subscribe to Our Newsletter to get Important News, <br /> Amazing
-          Offers and Inside Scoops:
-        </p>
+        <h2>{title}</h2>
+        <p>{subtitle}</p>
         <Form onEmailSubmit={handleEmail} />
       </div>
     </section>
@@ -29,3 +37,12 @@ export default function Cta() {
 // pridedam forma
 
 // padarom kad CTA butu pernaudojamas komponentas kuris pasiima tittle, subtitle ir is jo gaunam ivesta input
+
+// default props
+Cta.defaultProps = {
+  title: 'Subscribe Now',
+  subtitle:
+    'Subscribe to Our Newsletter to get Important News, Amazing Offers and Inside Scoops:',
+  bg: 'https://assets.codepen.io/6093409/river.mp4',
+};
+export default Cta;
